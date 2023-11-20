75d92cca36001f08a6676f544f4f7639ea76b526
chanwoo040531 <chanwoo040531@gmail.com>
2023-11-21 02:19:54 +0900

    [feat] create base entity

--- service/src/main/kotlin/me/chnu/treep/TreepApplication.kt
+++ service/src/main/kotlin/me/chnu/treep/TreepApplication.kt
@@ -2,8 +2,10 @@ package me.chnu.treep
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class TreepApplication
 
 fun main(args: Array<String>) {
