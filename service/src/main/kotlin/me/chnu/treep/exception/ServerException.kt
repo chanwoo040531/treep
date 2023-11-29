cf810fcdac61a69fa328abc0b293a7dfab4d3d14
chanwoo040531 <chanwoo040531@gmail.com>
2023-11-29 16:14:07 +0900

    [feat] define 'UnautorizaedException'

--- service/src/main/kotlin/me/chnu/treep/exception/ServerException.kt
+++ service/src/main/kotlin/me/chnu/treep/exception/ServerException.kt
@@ -9,3 +9,8 @@ internal sealed class ServerException(
 internal data class NotFoundException(
     override val message: String
 )  : ServerException(message = message)
+
+internal data class UnauthorizedException(
+    override val message: String = "잘못된 인증 정보입니다"
+)  : ServerException(message = message)
+
