23d9ac61bf4470d903d7297b446e1dc81da3b66a
chanwoo040531 <chanwoo040531@gmail.com>
2023-11-29 18:16:55 +0900

    [feat] create exception handler

---
+++ auth-service/src/main/kotlin/me/chnu/treep/exception/ExceptionHandler.kt
@@ -0,0 +1,26 @@
+package me.chnu.treep.exception
+
+import me.chnu.treep.presentation.ApiResponse
+import mu.KotlinLogging
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+internal class ExceptionHandler {
+    private val logger = KotlinLogging.logger {}
+
+    @ExceptionHandler(ServerException::class)
+    fun handleServerException(ex: ServerException): ResponseEntity<ApiResponse<Unit>> {
+        logger.error { ex.message }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(ApiResponse.error(message = ex.message))
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(ex: Exception): ResponseEntity<ApiResponse<Unit>> {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ApiResponse.error(message = "Internal Server Error"))
+    }
+}
