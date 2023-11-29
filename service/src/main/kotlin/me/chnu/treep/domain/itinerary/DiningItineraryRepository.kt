53f383e5831685157442f460a48b4441cd4b297a
chanwoo040531 <chanwoo040531@gmail.com>
2023-11-29 16:01:20 +0900

    [fix] fix itinerary flows

--- service/src/main/kotlin/me/chnu/treep/domain/itinerary/DiningItineraryRepository.kt
+++
@@ -1,7 +0,0 @@
-package me.chnu.treep.domain.itinerary
-
-import me.chnu.treep.domain.Key
-import me.chnu.treep.domain.itinerary.entity.DiningItinerary
-import org.springframework.data.jpa.repository.JpaRepository
-
-internal interface DiningItineraryRepository : JpaRepository<DiningItinerary, Key>
