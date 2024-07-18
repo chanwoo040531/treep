3e2c9c2638bc70cffad3ff55d794d83e0e4515cc
김찬우 <chanwoo040531@gmail.com>
2024-07-18 15:29:55 +0900

    [feat] remove unnecessary keywords from endpoints

--- service/src/main/kotlin/me/chnu/treep/presentation/api/TripPlanController.kt
+++ service/src/main/kotlin/me/chnu/treep/presentation/api/TripPlanController.kt
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/v1/plans")
+@RequestMapping("/v1/plans")
 internal class TripPlanController(
     private val tripPlanWriteService: TripPlanWriteService,
     private val tripPlanReadService: TripPlanReadService,
