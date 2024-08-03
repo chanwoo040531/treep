0db311c719cd28dd1ef113d38c54e19fcfe8d1a9
chanwoo040531 <chanwoo040531@gmail.com>
2024-08-03 23:51:25 +0900

    [chore] add kotest dependencies

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,13 +1,26 @@
 apply(plugin = "kotlin-jpa")
 
+val kotestVersion = "5.9.0"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     runtimeOnly("org.postgresql:postgresql")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+    // Serialization
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
+
+    // Test
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
