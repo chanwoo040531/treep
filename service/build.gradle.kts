2c426b8a99de4ac8e6646b9c791f6fb2df4ac60d
김찬우 <chanwoo040531@gmail.com>
2024-08-07 13:48:40 +0900

    [chore] update spring minor version (3.1.5 -> 3.3.2)

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,21 +1,31 @@
 apply(plugin = "kotlin-jpa")
 
+val bootVersion = "3.3.2"
+val jacksonVersion = "2.15.0"
 val kotestVersion = "5.9.0"
+val flywayVersion = "10.12.0"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-web:$bootVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-test$bootVersion")
+    implementation("org.springframework.boot:spring-boot-starter-actuator$bootVersion")
 
-    runtimeOnly("org.postgresql:postgresql")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    // Database
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:$bootVersion")
+    runtimeOnly("org.postgresql:postgresql:42.7.2")
+
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
+
+    // Docker
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose:$bootVersion")
 
     // Serialization
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
 
-    // Test
+    // Kotest
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.kotest:kotest-property:$kotestVersion")
