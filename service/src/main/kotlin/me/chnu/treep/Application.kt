928b74fc9c9da5a516a891353fc00c459ba25fde
김찬우 <chanwoo040531@gmail.com>
2024-08-07 13:29:29 +0900

    [refactor] renamed init class

--- service/src/main/kotlin/me/chnu/treep/Application.kt
+++ service/src/main/kotlin/me/chnu/treep/Application.kt
@@ -4,10 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@SpringBootApplication
 @EnableJpaAuditing
-class TreepApplication
+@SpringBootApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<TreepApplication>(*args)
+    runApplication<Application>(*args)
 }
