6941ae2d29f67706dfe03901ccdf99db3895b929
chanwoo040531 <chanwoo040531@gmail.com>
2024-08-07 11:52:54 +0900

    [feat] apply jackson config

--- service/src/main/kotlin/me/chnu/treep/config/JacksonConfig.kt
+++ service/src/main/kotlin/me/chnu/treep/config/JacksonConfig.kt
@@ -4,16 +4,21 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 internal class JacksonConfig {
 
     @Bean
-    fun objectMapper() = ObjectMapper().apply {
-        registerModule(JavaTimeModule())
-        disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-        disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-    }
+    @Primary
+    @ConditionalOnMissingBean
+    fun objectMapper(): ObjectMapper = ObjectMapper()
+        .registerKotlinModule()
+        .registerModule(JavaTimeModule())
+        .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 }
--- service/src/main/kotlin/me/chnu/treep/config/WebConfig.kt
+++ service/src/main/kotlin/me/chnu/treep/config/WebConfig.kt
@@ -1,29 +1,37 @@
 package me.chnu.treep.config
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import kotlinx.coroutines.runBlocking
-import me.chnu.treep.exception.UnauthorizedException
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.MethodParameter
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 internal class WebConfig(
     private val authUserHandlerArgumentResolver: AuthUserHandlerArgumentResolver,
+    private val objectMapper: ObjectMapper,
 )  : WebMvcConfigurationSupport() {
     override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
         argumentResolvers.apply {
             add(authUserHandlerArgumentResolver)
         }
     }
+
+    override fun extendMessageConverters(converters: List<HttpMessageConverter<*>>) =
+        converters.forEach {
+            if (it is MappingJackson2HttpMessageConverter) {
+                it.objectMapper = objectMapper
+                return
+            }
+        }
 }
 
 @Component
@@ -53,7 +61,6 @@ internal class AuthUserHandlerArgumentResolver(
 //                .awaitBody<AuthUser>()
 //        }
     }
-
 }
 
 data class AuthUser (
