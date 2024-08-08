86c40de574d21eae5e37f73a8fc2ff05e46fc020
김찬우 <chanwoo040531@gmail.com>
2024-08-08 14:11:30 +0900

    [fix] moved to `config/web`

---
+++ service/src/main/kotlin/me/chnu/treep/config/web/WebConfig.kt
@@ -0,0 +1,29 @@
+package me.chnu.treep.config.web
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+
+@Configuration(proxyBeanMethods = false)
+internal class WebConfig(
+    private val authUserHandlerArgumentResolver: AuthUserHandlerArgumentResolver,
+    private val objectMapper: ObjectMapper,
+) : WebMvcConfigurationSupport() {
+    override fun addArgumentResolvers(argumentResolvers: MutableList<HandlerMethodArgumentResolver>) {
+        argumentResolvers.apply {
+            add(authUserHandlerArgumentResolver)
+        }
+    }
+
+    override fun extendMessageConverters(converters: List<HttpMessageConverter<*>>) =
+        converters.forEach {
+            if (it is MappingJackson2HttpMessageConverter) {
+                it.objectMapper = objectMapper
+                return
+            }
+        }
+}
+
