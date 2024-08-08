2d92d79bb81e415e18d2743634f2d588d622683b
김찬우 <chanwoo040531@gmail.com>
2024-08-08 15:45:37 +0900

    [fix] chagne database (mysql -> postgres)

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -6,6 +6,13 @@ dependencies {
     testImplementation(libs.spring.boot.test)
     implementation(libs.spring.boot.data.redis)
     implementation(libs.spring.boot.validation)
+    runtimeOnly(libs.postgresql)
+
+    developmentOnly(libs.spring.boot.dockercompose)
+
+    implementation("com.auth0:java-jwt:4.4.0")
+
+    implementation(libs.kotlinlogging)
 
     // security
     implementation("org.springframework.boot:spring-boot-starter-security")
