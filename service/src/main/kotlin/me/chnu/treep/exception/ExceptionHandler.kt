0ec4e86a4a454b0646a41a5ba570f2acbba1ef23
김찬우 <chanwoo040531@gmail.com>
2024-08-08 15:46:20 +0900

    [refactor] refactor `ExceptionHandler`

--- service/src/main/kotlin/me/chnu/treep/exception/ExceptionHandler.kt
+++ service/src/main/kotlin/me/chnu/treep/exception/ExceptionHandler.kt
@@ -3,6 +3,7 @@ package me.chnu.treep.exception
 import io.github.oshai.kotlinlogging.KotlinLogging
 import me.chnu.treep.presentation.ApiResponse
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -11,16 +12,44 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 internal class ExceptionHandler {
     private val logger = KotlinLogging.logger {}
 
+    @ExceptionHandler(UnauthorizedException::class)
+    fun handleUnauthorizedException(ex: UnauthorizedException) =
+        responseEntity(
+            ex,
+            HttpStatus.UNAUTHORIZED,
+            ex.message
+        )
+
+    @ExceptionHandler(NotFoundException::class)
+    fun handleNotFoundException(ex: NotFoundException) =
+        responseEntity(
+            ex,
+            HttpStatus.NOT_FOUND,
+            ex.message
+        )
+
     @ExceptionHandler(ServerException::class)
-    fun handleServerException(ex: ServerException): ResponseEntity<ApiResponse<Unit>> {
-        logger.error { ex.message }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-            .body(ApiResponse.error(message = ex.message))
-    }
+    fun handleServerException(ex: ServerException) =
+        responseEntity(
+            ex,
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            ex.message
+        )
 
     @ExceptionHandler(Exception::class)
-    fun handleException(ex: Exception): ResponseEntity<ApiResponse<Unit>> {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(ApiResponse.error(message = "Internal Server Error"))
+    fun handleException(ex: Exception) =
+        responseEntity(
+            ex,
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            "Internal Server Error"
+        ).also { logger.error { ex.message } }
+
+    fun responseEntity(
+        ex: Throwable,
+        status: HttpStatusCode,
+        message: String
+    ): ResponseEntity<ApiResponse<Unit>> {
+        return ResponseEntity.status(status)
+            .body(ApiResponse.error(message = message))
     }
 }
