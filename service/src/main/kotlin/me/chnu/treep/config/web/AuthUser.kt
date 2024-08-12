3b41aee29b091370e1c313cffd9728ecd3776654
김찬우 <chanwoo040531@gmail.com>
2024-08-12 13:14:36 +0900

    [refactor] convert from interface to data class

--- service/src/main/kotlin/me/chnu/treep/config/web/AuthUser.kt
+++ service/src/main/kotlin/me/chnu/treep/config/web/AuthUser.kt
@@ -2,9 +2,9 @@ package me.chnu.treep.config.web
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-internal interface AuthUser {
+internal data class AuthUser(
     @get:JsonProperty("id")
-    val userId: Long
-    val username: String
+    val userId: Long,
+    val username: String,
     val expiresAt: Long
-}
+)
