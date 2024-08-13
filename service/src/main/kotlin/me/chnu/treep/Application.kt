1acaae9eb471a4df8158a98f6bbd51bb7017a14e
김찬우 <chanwoo040531@gmail.com>
2024-08-13 13:03:32 +0900

    [feat] enable aspectj auto proxy

--- service/src/main/kotlin/me/chnu/treep/Application.kt
+++ service/src/main/kotlin/me/chnu/treep/Application.kt
@@ -2,8 +2,10 @@ package me.chnu.treep
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableAspectJAutoProxy
 @EnableJpaAuditing
 @SpringBootApplication
 class Application
