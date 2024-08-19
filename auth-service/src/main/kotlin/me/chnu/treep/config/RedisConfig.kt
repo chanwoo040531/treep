5ab3965c9212025b130cbaaa822e9c57f45c4c98
김찬우 <chanwoo040531@gmail.com>
2024-08-19 09:15:28 +0900

    [feat] apply Lettuce implement

--- auth-service/src/main/kotlin/me/chnu/treep/config/RedisConfig.kt
+++ auth-service/src/main/kotlin/me/chnu/treep/config/RedisConfig.kt
@@ -1,16 +1,25 @@
 package me.chnu.treep.config
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.DependsOn
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 
 @Configuration
-internal class RedisConfig {
+internal class RedisConfig(
+    private val redisProperties: RedisProperties
+) {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory =
+        LettuceConnectionFactory(redisProperties.host, redisProperties.port)
 
     @Bean
+    @DependsOn("redisConnectionFactory")
     fun redisTemplate(factory: RedisConnectionFactory): RedisTemplate<String, Any> =
-        RedisTemplate<String, Any>().also {
-            it.connectionFactory = factory
+        RedisTemplate<String, Any>().apply {
+            connectionFactory = factory
         }
 }
