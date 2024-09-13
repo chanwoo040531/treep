10041ebfb20e762626db55c564a4b3c71d3ff67f
김찬우 <chanwoo040531@gmail.com>
2024-09-13 17:55:08 +0900

    [feat] create `WaypointType` enum

---
+++ plan-service/src/main/kotlin/me/chnu/treep/domain/itinerary/entity/WaypointType.kt
@@ -0,0 +1,10 @@
+package me.chnu.treep.domain.itinerary.entity
+
+enum class WaypointType {
+    PLACE,          // 장소
+    ACCOMMODATION,  // 숙박
+    RESTAURANT,     // 식당
+    SHOP,           // 쇼핑
+    ACTIVITY,       // 활동
+    ETC,            // 기타
+}
