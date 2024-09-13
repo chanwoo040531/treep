40da75c9c5db242d327fc4bc74b327a4ba720683
김찬우 <chanwoo040531@gmail.com>
2024-09-13 17:55:25 +0900

    [feat] create `RouteType` enum

---
+++ plan-service/src/main/kotlin/me/chnu/treep/domain/itinerary/entity/RouteType.kt
@@ -0,0 +1,15 @@
+package me.chnu.treep.domain.itinerary.entity
+
+enum class RouteType {
+    WALK,           // 도보
+    DRIVE,          // 자동차 운전
+    PUBLIC_TRANSIT, // 대중교통
+    TAXI,           // 택시
+    BICYCLE,        // 자전거
+    TRAIN,          // 기차
+    BUS,            // 버스
+    SUBWAY,         // 지하철
+    FERRY,          // 페리
+    PLANE,          // 비행기
+    ETC,            // 기타
+}
